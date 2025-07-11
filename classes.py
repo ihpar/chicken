a025aa2e77e36be7731c66ca44bc4423f3b103c8
ihpar <ismail.hakki.parlak@gmail.com>
2025-07-11 22:12:33 +0300

    init

---
+++ classes.py
@@ -0,0 +1,18 @@
+classes = ["Air",
+           "B 100", "B 75", "B 50", "B 25", "B 0"]
+colors = ["#2c73d2",
+          "#895002", "#a77718", "#c2a132", "#dacc4e", "#effa70"]
+AIR = 0
+BUTTER_100 = 1
+BUTTER_75 = 2
+BUTTER_50 = 3
+BUTTER_25 = 4
+BUTTER_0 = 5
+
+regression_targets = {
+    BUTTER_100: 100,
+    BUTTER_75: 75,
+    BUTTER_50: 50,
+    BUTTER_25: 25,
+    BUTTER_0: 0
+}
