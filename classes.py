14a05622a076289498d9303b4d9e760dfeb43933
ihpar <ismail.hakki.parlak@gmail.com>
2025-07-12 19:08:18 +0300

    adds day 1 data

--- classes.py
+++ classes.py
@@ -1,8 +1,10 @@
-classes = ["Air", "D 0"]
-colors = ["#2c73d2", "#895002"]
+classes = ["Air", "D 0", "D 1"]
+colors = ["#2c73d2", "#895002", "#895002"]
 AIR = 0
 DAY_0 = 1
+DAY_1 = 2
 
 regression_targets = {
-    DAY_0: 100
+    DAY_0: 100,
+    DAY_1: 100
 }
